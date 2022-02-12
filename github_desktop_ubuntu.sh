275f1a58bdbf7d1000986b014d207c571cac4487
Hou <houkihou@gmail.com>
2022-02-12 23:05:30 +0900

    1.0

---
+++ github_desktop_ubuntu.sh
@@ -0,0 +1,7 @@
+## Follow this link for further updates to Github Desktop for Ubuntu https://github.com/shiftkey/desktop/releases/latest
+# UPDATE (2021-10-18): Thanks to Amin Yahyaabadi's message, the updated code is as follows
+
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
+### Uncomment below line if you have not installed gdebi-core before
+sudo apt-get install gdebi-core 
+sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
